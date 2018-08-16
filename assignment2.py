ea3f3677f3e18b51bd24dd4f9acadf47043f993e
sharmasurbhi@gmail.com <sharmasurbhi1885@gmail.com>
2018-08-17 00:24:42 +0530

    this is my 3rd commit

---
+++ assignment2.py
@@ -0,0 +1,22 @@
+#Print anything you want on screen
+print('Hey! I am using Pycharm');
+#Join two strings using'+'.
+print("Be "+"Yourself");
+#Take the input of 3 variables x,y,z.print their values on the screen
+x=input("enter first number :");
+y=input("enter second number :");
+z=input("enter third number :");
+print(x);
+print(y);
+print(z);
+#print "Let's get started" on screen
+print("Let's get started");
+#Print the following values using placeholder:
+s="Acadview"
+course="python"
+fees=5000
+print("{} {} {}".format(s,course,fees));
+#Let's do some interesting exercise:
+name="Tony stark"
+salary=1000000
+print(('%s"%d')%(name,salary))
