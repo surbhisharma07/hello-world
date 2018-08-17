107eb98164368ec81fc1592952ff0855a80f8f1f
sharmasurbhi@gmail.com <sharmasurbhi1885@gmail.com>
2018-08-17 16:39:38 +0530

    this is my fourth commit

---
+++ practice_part_2.py
@@ -0,0 +1,22 @@
+#Print anything you want on screen
+print("Hey! I'm using Pycharm")
+#Join two strings using'+'.
+print("Be "+"Yourself")
+#Take the input of 3 variables x,y,z.print their values on the screen
+x=input("enter first number :")
+y=input("enter second number :")
+z=input("enter third number :")
+print("first Number is:",x)
+print("Second Number is:",y)
+print("Third Number is:",z)
+#print "Let's get started" on screen
+print("Let's get started")
+#Print the following values using placeholder:
+s="Acadview"
+course="python"
+fees=5000
+print("%s %s %d"%(s,course,fees))
+#Find the area of circle with user defined radius:
+r=int(input("enter the radius"))
+m=3.14*r*r
+print("radius of the circle:",m)
